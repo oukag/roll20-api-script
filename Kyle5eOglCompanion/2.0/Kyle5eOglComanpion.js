07692cbdfd6f8aae590186cee36dc45affbe2f7e
oukag <kylegraham110@gmail.com>
2017-03-29 10:50:49 -0400

    Readding spell functionality

--- Kyle5eOglCompanion/2.0/Kyle5eOglComanpion.js
+++ Kyle5eOglCompanion/2.0/Kyle5eOglComanpion.js
@@ -174,6 +174,175 @@ var Kyle5eOglCompanion = Kyle5eOglCompanion || (function(){
 		return obj;
 	}()),
 	
+	Spell = Spell || (function(){
+		var obj = {};
+			obj.name = "";
+			obj.level = "";
+			obj.school = "";
+			obj.castingtime = "";
+			obj.range = "";
+			obj.comp = "";
+			obj.comp_v = "";
+			obj.comp_s = "";
+			obj.comp_m = "";
+			obj.comp_materials = "";
+			obj.concentrationflag = "";
+			obj.concentration = "";
+			obj.duration = "";
+			obj.content = "";
+			obj.attack = ""
+			obj.damage = "";
+			obj.damagetype = "";
+			obj.damage_progression = "";
+			obj.prep = "";
+			obj.attackinfoflag = "";
+			obj.output = "";
+			obj.description = "";
+			obj.athigherlevels = "";
+			obj.attackid = "";
+			obj.rollcontent = "";
+			obj.options_flag = "";
+			obj.name_base = "";
+			obj.characterid = "";
+			obj.rowid = "";
+		
+		var repeating_spell_prefix = "repeating_spell-",
+		
+			athigherlevels_suffix = "_spellathigherlevels",
+			attackid_suffix = "_spellattackid",
+			attackinfoflag_suffix = "_spellattackinfoflag",
+			attack_suffix = "_spellattack",
+			castingtime_suffix = "_spellcastingtime",
+			comp_suffix = "_spellcomp",
+			comp_v_suffix = "_spellcomp_v",
+			comp_s_suffix = "_spellcomp_s",
+			comp_m_suffix = "_spellcomp_m",
+			comp_materials_suffix = "_spellcomp_materials",
+			concentrationflag_suffix = "_spellconcentrationflag",
+			concentration_suffix = "_spellconcentration",
+			content_suffix = "_spellcontent",
+			damage_progression_suffix = "_spell_damage_progression",
+			damage_suffix = "_spelldamage",
+			damagetype_suffix = "_spelldamagetype",
+			description_suffix = "_spelldescription",
+			duration_suffix = "_spellduration",
+			level_suffix = "_spelllevel",
+			name_base_suffix = "_spellname_base",
+			name_suffix = "_spellname",
+			output_suffix = "_spelloutput",
+			prep_suffix = "_prep",
+			range_suffix = "_spellrange",
+			rollcontent_suffix = "_rollcontent",
+			school_suffix = "_spellschool",
+		
+		/*
+		 * Should only be called after the characterid is set for the object.
+		 */
+		getAttrNotNull = function(n) {
+			var attr = GeneralScripts.FindAttrForCharacterId(obj.characterid, n);
+			return (attr != null) ? attr.get("current") : "";
+		},
+		
+		
+		update = function(spellevel,rowId, charId) {
+			var prefix = repeating_spell_prefix + ((spelllevel == "0") ? "cantrip" : spelllevel);
+			
+			obj.characterid = charId;
+			obj.rowId = rowId;
+			obj.level = spelllevel;
+			
+			obj.athigherlevels =     getAttrNotNull(prefix + athigherlevels_suffix);
+			obj.attackid =           getAttrNotNull(prefix + attackid_suffix);
+			obj.attackinfoflag =     getAttrNotNull(prefix + attackinfoflag_suffix);
+			obj.attack =             getAttrNotNull(prefix + attack_suffix);
+			obj.castingtime =        getAttrNotNull(prefix + castingtime_suffix);
+			obj.comp =               getAttrNotNull(prefix + comp_suffix);
+			obj.comp_v =             getAttrNotNull(prefix + comp_v_suffix);
+			obj.comp_s =             getAttrNotNull(prefix + comp_s_suffix);
+			obj.comp_m =             getAttrNotNull(prefix + comp_m_suffix);
+			obj.comp_materials =     getAttrNotNull(prefix + comp_materials_suffix);
+			obj.concentrationflag =  getAttrNotNull(prefix + concentrationflag_suffix);
+			obj.concentration =      getAttrNotNull(prefix + concentration_suffix);
+			obj.content =            getAttrNotNull(prefix + content_suffix);
+			obj.damage_progression = getAttrNotNull(prefix + damage_progression_suffix);
+			obj.damage =             getAttrNotNull(prefix + damage_suffix);
+			obj.damagetype =         getAttrNotNull(prefix + damagetype_suffix);
+			obj.description =        getAttrNotNull(prefix + description_suffix);
+			obj.duration =           getAttrNotNull(prefix + duration_suffix);
+			obj.name_base =          getAttrNotNull(prefix + name_base_suffix);
+			obj.name =               getAttrNotNull(prefix + name_suffix);
+			obj.options_flag =       getAttrNotNull(prefix + options_flag_suffix);
+			obj.output =             getAttrNotNull(prefix + output_suffix);
+			obj.prep =               getAttrNotNull(prefix + prep_suffix);
+			obj.range =              getAttrNotNull(prefix + range_suffix);
+			obj.rollcontent =        getAttrNotNull(prefix + rollcontent_suffix);
+			obj.school =             getAttrNotNull(prefix + school_suffix);
+			return obj;
+		};
+		
+		obj.set =function(n,v) {
+			var suffix = null;
+			switch (n) {
+				case "athigherlevels":     obj.athigherlevels = v;     suffix = athigherlevels_suffix;     break;
+				case "attackid":           obj.attackid = v;           suffix = attackid_suffix;           break;
+				case "attackinfoflag":     obj.attackinfoflag = v;     suffix = attackinfoflag_suffix;     break;
+				case "attack":             obj.attack = v;             suffix = attack_suffix;             break;
+				case "castingtime":        obj.castingtime = v;        suffix = castingtime_suffix;        break;
+				case "comp_v":             obj.comp_v = v;             suffix = comp_v_suffix;             break;
+				case "comp_s":             obj.comp_s = v;             suffix = comp_s_suffix;             break;
+				case "comp_m":             obj.comp_m = v;             suffix = comp_m_suffix;             break;
+				case "comp_materials":     obj.comp_materials = v;     suffix = comp_materials_suffix;     break;
+				case "concentrationflag":  obj.concentrationflag = v;  suffix = concentrationflag_suffix;  break;
+				case "concentration":      obj.concentration = v;      suffix = concentration_suffix;      break;
+				case "content":            obj.content = v;            suffix = content_suffix;            break;
+				case "damage_progression": obj.damage_progression = v; suffix = damage_progression_suffix; break;
+				case "damage":             obj.damage = v;             suffix = damage_suffix;             break;
+				case "damagetype":         obj.damagetype = v;         suffix = damagetype_suffix;         break;
+				case "description":        obj.description = v;        suffix = description_suffix;        break;
+				case "duration":           obj.duration = v;           suffix = duration_suffix;           break;
+				case "level":              obj.level = v;              suffix = level_suffix;              break;
+				case "name_base":          obj.name_base = v;          suffix = name_base;                 break;
+				case "name":               obj.name = v;               suffix = name;                      break;
+				case "options_flag":       obj.options_flag = v;       suffix = options_flag_suffix;       break;
+				case "output":             obj.output = v;             suffix = output_suffix;             break;
+				case "prep":               obj.prep = v;               suffix = prep_suffix;               break;
+				case "range":              obj.range = v;              suffix = range_suffix;              break;
+				case "rollcontent":        obj.rollcontent = v;        suffix = rollcontent_suffix;        break;
+				case "school":             obj.school = v;             suffix = school_suffix;             break;
+				default:
+					log("Could not find attribute '" + n + "' for spell. It may be 'private' or not exist");
+					break;
+			}
+			if(suffix) {
+				var attr = GeneralScripts.FindAttrForCharacterId(obj.characterid, repeating_spell_prefix + obj.spelllevel + suffix);
+				if(!attr) {
+					attr = createObj ("attribute", {
+						name: repeating_spell_prefix + obj.spelllevel + suffix,
+						characterid: obj.characterid,
+					});
+				}
+				attr.set("current", v);
+			}
+			return obj;
+		};
+		
+		obj.getForSpelllevelAndRowId = function(spelllevel, rowId, charId){
+			var error = null;
+			var prefix = repeating_spell_prefix + ((spelllevel == "0") ? "cantrip" : spelllevel);
+			// See if the spell for the given row id and spell level exists
+			var spellname_baseAttr = GeneralScripts.FindAttrForCharacterId(charId, prefix + name_base_suffix);
+			if(!spellname_baseAttr) {
+				error = "Could not find spell for level '" + spelllevel + "' and row id '" + rowId + "' for character with id '" + charId + "'";
+				GeneralScripts.WhisperError(scriptName, error);
+				return null;
+			}
+			
+			return update(spelllevel,rowId,charId);
+		};
+		
+		return obj;
+	}()),
+	
 	RollTemplate_Simple = RollTemplate_Simple || (function(){
 		var obj = {};
 			obj.rname = null;
